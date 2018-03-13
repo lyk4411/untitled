c72f27f985574a9eb3e01f542ea5581ed5b575f8
ykliu <moneyflying_2006@hotmail.com>
2018-03-13 14:49:37 +0800

    learn python

--- beginPython/ch09/flatten.py
+++ beginPython/ch09/flatten.py
@@ -1,11 +1,14 @@
+from collections import deque
 def flatten(nested):
     try:
         for sublist in nested:
             for element in flatten(sublist):
-                yield element,"a"
+                yield element
 
     except TypeError:
-        yield nested,"b"
+        yield nested
 
 
 print(list(flatten([[[1],2],3,4,[5,[6,7]],8])))
+print(tuple(flatten([[[1],2],3,4,[5,[6,7]],8])))
+print(deque(flatten([[[1],2],3,4,[5,[6,7]],8])))
