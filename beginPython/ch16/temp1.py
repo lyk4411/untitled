b34805e997617ef4d531e175ccf4506482f98183
ykliu <moneyflying_2006@hotmail.com>
2018-03-21 15:03:15 +0800

    learn python

---
+++ beginPython/ch16/temp1.py
@@ -0,0 +1,13 @@
+def square(x):
+    """
+    >>> square(2)
+    4
+    >>> square(3)
+    9
+    """
+    return x*x
+
+
+if __name__ == '__main__':
+    import doctest,temp1
+    doctest.testmod(temp1)
---
+++ beginPython/ch16/temp2.py
@@ -0,0 +1,4 @@
+
+if __name__ == '__main__':
+    import doctest,temp1
+    doctest.testmod(temp1)
