5ef2dd87140f074d3362f853ee6a3168ba6939f4
ykliu <moneyflying_2006@hotmail.com>
2018-03-22 18:42:33 +0800

    learn python

---
+++ beginPython/ch18/hello.py
@@ -0,0 +1,14 @@
+from pip._vendor.distlib.compat import raw_input
+
+
+def hello():
+    print('hello world.......')
+    raw_input('Press <enter>')
+    print('hello world.......')
+
+
+
+if __name__ == "__main__":
+    hello()
+
+
---
+++ beginPython/ch18/temp1.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='HelloWorld',
+      version='1.0',
+      description='A example',
+      author='ykliu',
+      py_modules=['hello'])
+
