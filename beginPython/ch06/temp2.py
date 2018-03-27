74e23f54170ce8bfb78da32a57c2969da3b31b53
ykliu <moneyflying_2006@hotmail.com>
2018-03-27 13:32:26 +0800

    learn python

---
+++ beginPython/ch06/temp2.py
@@ -0,0 +1,24 @@
+import re,sys
+
+field_pat = re.compile(r'\[(.+?)\]')
+
+scope = {}
+def replacement(match):
+    code = match.group(1)
+    try:
+        return str(eval(code,scope))
+    except SyntaxError:
+        exec (code,scope)
+        print(code)
+        return '********'
+
+
+lines = []
+fp = "[x=2]" \
+     "[y=3]" \
+     "the sum of [x] and [y] is [x+y]"
+for line in fp:
+    lines.append(line)
+
+text = ''.join(lines)
+print(field_pat.sub(replacement,text))
