87f360f941f52142d42c987be3a0fbcc999b654e
ykliu <moneyflying_2006@hotmail.com>
2018-03-27 11:04:51 +0800

    learn python

--- beginPython/ch06/temp1.py
+++ beginPython/ch06/temp1.py
@@ -1,3 +1,28 @@
 import re
 inputStr = "hello crifan, nihao crifan";
 print(re.sub(r"hello (\w+), nihao \1", "crifanli", inputStr))
+
+inputStr = "hello crifan, nihao crifan";
+replacedStr = re.sub(r"hello (\w+), nihao \1", "\g<1>", inputStr);
+print ("replacedStr=",replacedStr) #crifan
+
+
+def pythonReSubDemo():
+    """
+        demo Pyton re.sub
+    """
+    inputStr = "hello 123 world 456";
+
+    def _add111(matched):
+        intStr = matched.group("number"); #123
+        intValue = int(intStr);
+        addedValue = intValue + 111; #234
+        addedValueStr = str(addedValue);
+        return addedValueStr;
+
+    replacedStr = re.sub("(?P<number>\d+)", _add111, inputStr);
+    print ("replacedStr=",replacedStr) #hello 234 world 567
+
+###############################################################################
+if __name__=="__main__":
+    pythonReSubDemo();
