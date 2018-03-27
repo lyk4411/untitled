33bdcac5b5cea9e906bf09a489c16e96a24ba941
ykliu <moneyflying_2006@hotmail.com>
2018-03-27 11:16:13 +0800

    learn python

--- beginPython/ch06/temp1.py
+++ beginPython/ch06/temp1.py
@@ -26,3 +26,26 @@ def pythonReSubDemo():
 ###############################################################################
 if __name__=="__main__":
     pythonReSubDemo();
+
+
+
+
+def pythonReSubDemo():
+    """
+        demo Pyton re.sub
+    """
+    inputStr = "hello 123 world 456 nihao 789";
+
+    def _add111(matched):
+        intStr = matched.group("number"); #123
+        intValue = int(intStr);
+        addedValue = intValue + 111; #234
+        addedValueStr = str(addedValue);
+        return addedValueStr;
+
+    replacedStr = re.sub("(?P<number>\d+)", _add111, inputStr, 2);
+    print ("replacedStr=",replacedStr) #hello 234 world 567 nihao 789
+
+###############################################################################
+if __name__=="__main__":
+    pythonReSubDemo();
