dce77059d19fa87feaea3f7fc16f436ec54901f7
ykliu <moneyflying_2006@hotmail.com>
2018-04-02 18:39:19 +0800

    learn python

---
+++ beginPython/ch23/temp.py
@@ -0,0 +1,9 @@
+from nntplib import NNTP
+server = NNTP('alibaba.dds.nl')
+server.group('ecs.pub.buildset')[0]
+
+
+
+# cedar.liii.com -- 1 group: liii.adsonline
+# excalibur.infoview.co.za -- 2 groups: ecs.pub.billcost and ecs.pub.buildset
+
