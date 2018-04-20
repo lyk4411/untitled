e12f4ccd7217caf729c23adcdb59e189a49cbd36
ykliu <moneyflying_2006@hotmail.com>
2018-04-20 15:13:04 +0800

    learn python

---
+++ beginPython/leetcode/SelfDividingNumbers.py
@@ -0,0 +1,28 @@
+
+
+class SelfDividingNumbers(object):
+    def selfDividingNumbers(self, left, right):
+        def self_dividing(n):
+            for d in str(n):
+                if d == '0' or n % int(d) > 0:
+                    return False
+            return True
+        """
+        Alternate implementation of self_dividing:
+        def self_dividing(n):
+            x = n
+            while x > 0:
+                x, d = divmod(x, 10)
+                if d == 0 or n % d > 0:
+                    return False
+            return True
+        """
+        ans = []
+        for n in range(left, right + 1):
+            if self_dividing(n):
+                ans.append(n)
+        return ans #Equals filter(self_dividing, range(left, right+1))
+
+if __name__ == '__main__':
+    a = SelfDividingNumbers()
+    print(a.selfDividingNumbers(1, 22))
