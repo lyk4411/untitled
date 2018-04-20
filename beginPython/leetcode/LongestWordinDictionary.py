2e239f55dbab7c4bf10c8437f85c0954f47a5186
ykliu <moneyflying_2006@hotmail.com>
2018-04-20 18:03:00 +0800

    learn python

--- beginPython/leetcode/LongestWordinDictionary.py
+++ beginPython/leetcode/LongestWordinDictionary.py
@@ -11,7 +11,18 @@ class LongestWordinDictionary(object):
                     ans = word
         return ans
 
+    def longestWord1(self, words):
+        wordset = set(words)
+        words.sort(key=lambda c: (-len(c), c))
+        for word in words:
+            if all(word[:k] in wordset for k in xrange(1, len(word))):
+                return word
+
+
 if __name__ == '__main__':
     a = LongestWordinDictionary()
     print(a.longestWord(["w", "wo", "wor", "worl", "world"]))
     print(a.longestWord(["a", "banana", "app", "appl", "ap", "apply", "apple"]))
+
+    print(a.longestWord1(["w", "wo", "wor", "worl", "world"]))
+    print(a.longestWord1(["a", "banana", "app", "appl", "ap", "apply", "apple"]))
