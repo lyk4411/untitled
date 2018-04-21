4ea996e800c3b58bdb0f3a3cce1dd3c1cc7e469d
ykliu <moneyflying_2006@hotmail.com>
2018-04-21 20:13:15 +0800

    learn python

--- beginPython/leetcode/PartitionLabels.py
+++ beginPython/leetcode/PartitionLabels.py
@@ -4,6 +4,9 @@
 class PartitionLabels(object):
     def partitionLabels(self, S):
         last = {c: i for i, c in enumerate(S)}
+        # temp = tuple((c, i) for i, c in enumerate(S))
+        # print(last)
+        # print(temp)
         j = anchor = 0
         ans = []
         for i, c in enumerate(S):
