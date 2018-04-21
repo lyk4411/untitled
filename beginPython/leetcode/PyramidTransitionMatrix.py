697ff2a26f59d17fc6fd3a06492376388a4dd16d
ykliu <moneyflying_2006@hotmail.com>
2018-04-21 21:20:48 +0800

    learn python

---
+++ beginPython/leetcode/PyramidTransitionMatrix.py
@@ -0,0 +1,36 @@
+class PyramidTransitionMatrix(object):
+    def pyramidTransition(self, bottom, allowed):
+        from collections import defaultdict
+        """
+        :type bottom: str
+        :type allowed: List[str]
+        :rtype: bool
+        """
+        mem = defaultdict(list)
+        for allow in allowed:
+            mem[allow[0:2]].append(allow[2])
+
+        dp = [[False] * 10 for i in range(20)]
+        n  = len(bottom)
+        for i in range(n):
+            dp[i][ord(bottom[i]) - ord('A')] = True
+
+        for i in range(n - 1, 0, -1):
+            ndp = [[False] * 10 for i in range(20)]
+            for j in range(i):
+                for l in range(7):
+                    for r in range(7):
+                        if (dp[j][l] and dp[j + 1][r]):
+                            if str(chr(65 + l) + "" + chr(65 + r)) in mem:
+                                for c in mem[chr(65 + l) + "" + chr(65 + r)]:
+                                    ndp[j][ord(c) - ord('A')] = True
+            dp = ndp
+        for i in range(7):
+            if (dp[0][i]): return True
+        return False
+
+if __name__ == '__main__':
+    a = PyramidTransitionMatrix()
+    bottom = r"XXYX"
+    allowed = [r"XXX", r"XXY", r"XYX", r"XYY", r"YXZ"]
+    print(a.pyramidTransition(bottom, allowed))
