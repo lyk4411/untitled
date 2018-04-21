1d7a6d376e2eb6e4ffea0b317ae42b630c2c9d90
ykliu <moneyflying_2006@hotmail.com>
2018-04-21 09:46:45 +0800

    learn python

---
+++ beginPython/leetcode/ImplementMagicDictionary.py
@@ -0,0 +1,14 @@
+import collections
+
+
+class ImplementMagicDictionary(object):
+    def __init__(self):
+        self.buckets = collections.defaultdict(list)
+
+    def buildDict(self, words):
+        for word in words:
+            self.buckets[len(word)].append(word)
+
+    def search(self, word):
+        return any(sum(a!=b for a,b in zip(word, candidate)) == 1
+                   for candidate in self.buckets[len(word)])
