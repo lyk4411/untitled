a5fe23b136e0da4195ed0d1034d9636de5419cd9
ykliu <moneyflying_2006@hotmail.com>
2018-04-21 20:07:44 +0800

    learn python

---
+++ beginPython/leetcode/PartitionLabels.py
@@ -0,0 +1,19 @@
+
+
+
+class PartitionLabels(object):
+    def partitionLabels(self, S):
+        last = {c: i for i, c in enumerate(S)}
+        j = anchor = 0
+        ans = []
+        for i, c in enumerate(S):
+            j = max(j, last[c])
+            if i == j:
+                ans.append(i - anchor + 1)
+                anchor = i + 1
+
+        return ans
+
+if __name__ == '__main__':
+    a = PartitionLabels()
+    print(a.partitionLabels(r"ababcbacadefegdehijhklij"))
