9e54e6c1ef1f8bf2e3ba0a693079f869e65244d4
ykliu <moneyflying_2006@hotmail.com>
2018-04-23 19:19:31 +0800

    learn python

--- beginPython/leetcode/MaximumLengthofPairChain.py
+++ beginPython/leetcode/MaximumLengthofPairChain.py
@@ -1,17 +1,26 @@
+import operator
 
 
-class MaximumLengthofPairChain(object): #Time Limit Exceeded
+class MaximumLengthofPairChain(object):
+    # def findLongestChain(self, pairs):  #Time Limit Exceeded
+    #     pairs.sort()
+    #     dp = [1] * len(pairs)
+    #
+    #     for j in range(len(pairs)):
+    #         for i in range(j):
+    #             if pairs[i][1] < pairs[j][0]:
+    #                 dp[j] = max(dp[j], dp[i] + 1)
+    #
+    #     return max(dp)
     def findLongestChain(self, pairs):
-        pairs.sort()
-        dp = [1] * len(pairs)
-
-        for j in range(len(pairs)):
-            for i in range(j):
-                if pairs[i][1] < pairs[j][0]:
-                    dp[j] = max(dp[j], dp[i] + 1)
-
-        return max(dp)
+        cur, ans = float('-inf'), 0
+        for x, y in sorted(pairs, key=operator.itemgetter(1)):
+            print(x,y)
+            if cur < x:
+                cur = y
+                ans += 1
+        return ans
 
 if __name__ == '__main__':
     a = MaximumLengthofPairChain()
-    print(a.findLongestChain([[1,2], [2,3], [3,4]]))
+    print(a.findLongestChain([[1,2], [3,4], [2,3]]))
