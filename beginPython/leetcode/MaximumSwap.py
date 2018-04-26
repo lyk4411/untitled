3d2d400e89983c73d265b7dd57130da995a35541
ykliu <moneyflying_2006@hotmail.com>
2018-04-26 08:11:12 +0800

    learn python

--- beginPython/leetcode/MaximumSwap.py
+++ beginPython/leetcode/MaximumSwap.py
@@ -3,21 +3,21 @@
 
 class MaximumSwap(object):
     def maximumSwap(self, num):
-        """
-        :type num: int
-        :rtype: int
-        """
         A = map(int, str(num))
+        B = list(str(num))
         last = {x: i for i, x in enumerate(A)}
-        for i, x in enumerate(A):
-            for d in range(9, x, -1):
-                if last.get(d, None) > i:
-                    A[i], A[last[d]] = A[last[d]], A[i]
-                    return int("".join(map(str, A)))
+        for i in range(len(str(num))):
+            for d in range(9, int(B[i]), -1):
+                if last.get(d, 0) > i:
+                    B[i], B[last[d]] = B[last[d]], B[i]
+                    return int("".join(map(str, B)))
         return num
 
 if __name__ == '__main__':
     a = MaximumSwap()
     print(a.maximumSwap(789))
+    print(a.maximumSwap(2736))
+    print(a.maximumSwap(21))
+    print(a.maximumSwap(12))
     print(a.maximumSwap(987654))
     print(a.maximumSwap(789452))
