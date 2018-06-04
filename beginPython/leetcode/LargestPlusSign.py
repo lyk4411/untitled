a429e5f75b18aa2ce3a522bb5935f15e90848dd6
ykliu <moneyflying_2006@hotmail.com>
2018-06-04 18:47:52 +0800

    learn python

---
+++ beginPython/leetcode/LargestPlusSign.py
@@ -0,0 +1,37 @@
+
+class LargestPlusSign(object):
+    def orderOfLargestPlusSign(self, N, mines):
+        banned = {tuple(mine) for mine in mines}
+        dp = [[0] * N for _ in range(N)]
+        ans = 0
+
+        for r in range(N):
+            count = 0
+            for c in range(N):
+                count = 0 if (r, c) in banned else count + 1
+                dp[r][c] = count
+
+            count = 0
+            for c in range(N - 1, -1, -1):
+                count = 0 if (r, c) in banned else count + 1
+                if count < dp[r][c]: dp[r][c] = count
+
+        for c in range(N):
+            count = 0
+            for r in range(N):
+                count = 0 if (r, c) in banned else count + 1
+                if count < dp[r][c]: dp[r][c] = count
+
+            count = 0
+            for r in range(N - 1, -1, -1):
+                count = 0 if (r, c) in banned else count + 1
+                if count < dp[r][c]: dp[r][c] = count
+                if dp[r][c] > ans: ans = dp[r][c]
+
+        return ans
+
+if __name__ == '__main__':
+    a = LargestPlusSign()
+    print(a.orderOfLargestPlusSign(5, [[4, 2]]))
+    print(a.orderOfLargestPlusSign(2, [[]]))
+    print(a.orderOfLargestPlusSign(1, [[0, 0]]))
