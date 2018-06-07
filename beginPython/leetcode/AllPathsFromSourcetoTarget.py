1cadea51e8b3a3fbd186a130ce8f9382325c9968
ykliu <moneyflying_2006@hotmail.com>
2018-06-07 15:30:20 +0800

    learn python

--- beginPython/leetcode/AllPathsFromSourcetoTarget.py
+++ beginPython/leetcode/AllPathsFromSourcetoTarget.py
@@ -11,7 +11,24 @@ class AllPathsFromSourcetoTarget(object):
             return ans
 
         return solve(0)
+    # def allPathsSourceTarget(self, graph):
+    #     """
+    #     :type graph: List[List[int]]
+    #     :rtype: List[List[int]]
+    #     """
+    #     res = []
+    #     self.dfs(graph, res, 0, [0])
+    #     return res
+    #
+    # def dfs(self, graph, res, pos, path):
+    #     if pos == len(graph) - 1:
+    #         res.append(path)
+    #
+    #     else:
+    #         for n in graph[pos]:
+    #             self.dfs(graph, res, n, path + [n])
 
 if __name__ == '__main__':
     a = AllPathsFromSourcetoTarget()
-    print(a.allPathsSourceTarget([[1,2], [3], [3], []]))
+    print(a.allPathsSourceTarget([[1, 2], [3], [3], []]))
+    print(a.allPathsSourceTarget([[1, 2], [3], [3], [4, 5], [6], [6], []]))
