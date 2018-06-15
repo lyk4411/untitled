b124867b906feeb1e753d1e46b43971f58a85a13
ykliu <moneyflying_2006@hotmail.com>
2018-06-15 09:36:29 +0800

    learn python

--- beginPython/leetcode/AmbiguousCoordinates.py
+++ beginPython/leetcode/AmbiguousCoordinates.py
@@ -1,25 +1,32 @@
 import itertools
 
+import copy
+
 
 class AmbiguousCoordinates(object):
     def ambiguousCoordinates(self, S):
         def make(frag):
             N = len(frag)
+            ans =[]
             for d in range(1, N + 1):
                 left = frag[:d]
                 right = frag[d:]
                 if ((not left.startswith('0') or left == '0')
                     and (not right.endswith('0'))):
-                    yield left + ('.' if d != N else '') + right
+                    ans.append(copy.deepcopy(left + ('.' if d != N else '') + right))
+            return ans
 
         S = S[1:-1]
-        return ["({}, {})".format(*cand)
-                for i in range(1, len(S))
-                for cand in itertools.product(make(S[:i]), make(S[i:]))]
+        ans = []
+        for i in range(1, len(S)):
+            ans.extend(tuple(itertools.product(make(S[:i]), make(S[i:]))))
+
+        r = ["({}, {})".format(*x) for x in ans]
+        return r
 
 if __name__ == '__main__':
     a = AmbiguousCoordinates()
     print(a.ambiguousCoordinates("(123)"))
-    print(a.ambiguousCoordinates("(00111)"))
+    # print(a.ambiguousCoordinates("(00111)"))
     print(a.ambiguousCoordinates("(0123)"))
-    print(a.ambiguousCoordinates("(100)"))
+    # print(a.ambiguousCoordinates("(100)"))
