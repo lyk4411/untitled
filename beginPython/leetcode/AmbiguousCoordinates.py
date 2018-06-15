e6baff4d6254167a94d3daf146193661ddcdb9c4
ykliu <moneyflying_2006@hotmail.com>
2018-06-15 09:38:26 +0800

    learn python

--- beginPython/leetcode/AmbiguousCoordinates.py
+++ beginPython/leetcode/AmbiguousCoordinates.py
@@ -4,25 +4,40 @@ import copy
 
 
 class AmbiguousCoordinates(object):
+    # def ambiguousCoordinates(self, S):
+    #     def make(frag):
+    #         N = len(frag)
+    #         ans =[]
+    #         for d in range(1, N + 1):
+    #             left = frag[:d]
+    #             right = frag[d:]
+    #             if ((not left.startswith('0') or left == '0')
+    #                 and (not right.endswith('0'))):
+    #                 ans.append(copy.deepcopy(left + ('.' if d != N else '') + right))
+    #         return ans
+    #
+    #     S = S[1:-1]
+    #     ans = []
+    #     for i in range(1, len(S)):
+    #         ans.extend(tuple(itertools.product(make(S[:i]), make(S[i:]))))
+    #
+    #     r = ["({}, {})".format(*x) for x in ans]
+    #     return r
+
     def ambiguousCoordinates(self, S):
         def make(frag):
             N = len(frag)
-            ans =[]
             for d in range(1, N + 1):
                 left = frag[:d]
                 right = frag[d:]
                 if ((not left.startswith('0') or left == '0')
                     and (not right.endswith('0'))):
-                    ans.append(copy.deepcopy(left + ('.' if d != N else '') + right))
-            return ans
+                    yield left + ('.' if d != N else '') + right
 
         S = S[1:-1]
-        ans = []
-        for i in range(1, len(S)):
-            ans.extend(tuple(itertools.product(make(S[:i]), make(S[i:]))))
-
-        r = ["({}, {})".format(*x) for x in ans]
-        return r
+        return ["({}, {})".format(*cand)
+                for i in range(1, len(S))
+                for cand in itertools.product(make(S[:i]), make(S[i:]))]
 
 if __name__ == '__main__':
     a = AmbiguousCoordinates()
