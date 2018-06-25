330f8f18887da614d7f7ca20d6edfa7192de7c0b
ykliu <moneyflying_2006@hotmail.com>
2018-06-25 19:23:50 +0800

    learn python

--- beginPython/ch27/temp7.py
+++ beginPython/ch27/temp7.py
@@ -5,7 +5,7 @@ otherpeer.hello('http://127.0.0.1:4243')
 # code,data = otherpeer.query('temp2.py')
 # print(code)
 # print(data)
-code,data = otherpeer.fetch('temp2.py', 'ccc')
+print(otherpeer.fetch('temp2.py', 'ccc'))
 
 # otherpeer.hello('http://127.0.0.1:4242')
 # otherpeer.fetch('test.txt','654321')
