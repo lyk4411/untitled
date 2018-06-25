3baed54a9d340497b6801af9cd909fe6c6f3dea2
ykliu <moneyflying_2006@hotmail.com>
2018-06-25 19:24:32 +0800

    learn python

--- beginPython/ch27/temp7.py
+++ beginPython/ch27/temp7.py
@@ -10,4 +10,6 @@ print(otherpeer.fetch('temp2.py', 'ccc'))
 # otherpeer.hello('http://127.0.0.1:4242')
 # otherpeer.fetch('test.txt','654321')
 
-
+# C:\Users\lyk\AppData\Local\Programs\Python\Python36> python  F:/Users/lyk/Pychar
+# mProjects/untitled/beginPython/ch27/temp1.py http://127.0.0.1:4242 F:\Users\lyk\
+# PycharmProjects\untitled\beginPython\ch27\c ccc
