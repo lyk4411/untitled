ff4927316943282d4344039b66e0adeba291e05c
ykliu <moneyflying_2006@hotmail.com>
2018-06-25 19:23:22 +0800

    learn python

--- beginPython/ch27/temp6.py
+++ beginPython/ch27/temp6.py
@@ -1,11 +1,16 @@
 from xmlrpc.client import ServerProxy
 
-mypeer = ServerProxy('http://127.0.0.1:4242')
-mypeer.hello('http://127.0.0.1:4243')
+mypeer = ServerProxy('http://localhost:4243')
+# mypeer.hello('http://127.0.0.1:4243')
 
 code,data = mypeer.query('test.txt')
 print(code)
 print(data)
 
-print(mypeer.fetch('test.txt',123456))
+code,data = mypeer.query('temp1.py')
+print(code)
+print(data)
+
+
+# print(mypeer.fetch('test.txt',123456))
 
--- beginPython/ch27/temp7.py
+++ beginPython/ch27/temp7.py
@@ -1,8 +1,12 @@
 from xmlrpc.client import ServerProxy
-otherpeer  = ServerProxy('http://127.0.0.1:4243')
-code,data = otherpeer.query('test.txt')
-print(code)
-print(data)
+otherpeer  = ServerProxy('http://127.0.0.1:4242')
+otherpeer.hello('http://127.0.0.1:4243')
+
+# code,data = otherpeer.query('temp2.py')
+# print(code)
+# print(data)
+code,data = otherpeer.fetch('temp2.py', 'ccc')
+
 # otherpeer.hello('http://127.0.0.1:4242')
 # otherpeer.fetch('test.txt','654321')
 
