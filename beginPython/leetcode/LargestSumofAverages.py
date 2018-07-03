5409d30b3e55411ccd113e63324ac7d8e80caa68
ykliu <moneyflying_2006@hotmail.com>
2018-07-03 10:37:02 +0800

    learn python

--- beginPython/leetcode/LargestSumofAverages.py
+++ beginPython/leetcode/LargestSumofAverages.py
@@ -10,14 +10,17 @@ class LargestSumofAverages(object):
         for x, a in enumerate(A):
             S[x + 1] += S[x] + a
 
-        dp = [1.0 * S[x] / x for x in range(1, N + 1)]
-        for x in range(K - 1):
-            dp0 = [0] * N
-            for y in range(x, N):
-                for z in range(x, y):
-                    dp0[y] = max(dp0[y], dp[z] + 1.0 * (S[y + 1] - S[z + 1]) / (y - z))
-            dp = dp0
-        return dp[-1]
+        dp = [[0 for x in range(N)] for y in range(K)]
+        for k in range(K):
+            for i in range(0, N):
+                if k == 0:
+                    dp[k][i] = S[i + 1]/(i + 1)
+                else:
+                    dp[k][i] = dp[k - 1][i]
+                if k > 0:
+                    for j in range(i - 1, -1, -1):
+                        dp[k][i] = max(dp[k][i], dp[k - 1][j] + (S[i + 1] - S[j + 1]) / (i - j))
+        return dp[K - 1][N - 1]
 
 if __name__ == '__main__':
     a = LargestSumofAverages()
