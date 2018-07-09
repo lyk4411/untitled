6155c89388f8aaae9c828fda0e0ccb0305918e1d
ykliu <moneyflying_2006@hotmail.com>
2018-07-09 18:09:48 +0800

    learn python

--- beginPython/ch27/temp7.py
+++ beginPython/ch27/temp7.py
@@ -2,12 +2,14 @@ from xmlrpc.client import ServerProxy
 otherpeer  = ServerProxy('http://127.0.0.1:4242')
 otherpeer.hello('http://127.0.0.1:4243')
 
-# code,data = otherpeer.query('temp2.py')
-# print(code)
-# print(data)
+code,data = otherpeer.query('temp2.py')
+print(code)
+print(data)
 print(otherpeer.fetch('temp2.py', 'ccc'))
 
 # otherpeer.hello('http://127.0.0.1:4242')
 # otherpeer.fetch('test.txt','654321')
 
 # C:\Users\lyk\AppData\Local\Programs\Python\Python36> python  F:/Users/lyk/PycharmProjects/untitled/beginPython/ch27/temp1.py http://127.0.0.1:4242 F:\Users\lyk\PycharmProjects\untitled\beginPython\ch27\c ccc
+
+# python  F:/Users/lyk/PycharmProjects/untitled/beginPython/ch27/temp1.py http://127.0.0.1:4243 F:\Users\lyk\PycharmProjects\untitled\beginPython\ch27\a  aaa
