631a0542bea962a2f91bd0580c911d60edf67599
ykliu <moneyflying_2006@hotmail.com>
2018-07-12 18:58:32 +0800

    learn python

--- beginPython/leetcode/StrangePrinter.py
+++ beginPython/leetcode/StrangePrinter.py
@@ -3,21 +3,17 @@ class StrangePrinter(object):
         size = len(s)
         if size <= 1:
             return size
-        dp = [[0 for j in range(100)] for i in range(100)]
+        dp = [[0 for j in range(size)] for i in range(size)]
         for i in range(size):
             for j in range(i, size):
                 dp[i][j] = j - i + 1
         for i in range(1, size):
-            j = 0
-            while j + i < size:
-                k = j
-                while k < j + i:
+            for j in range(size - i):
+                for k in range(j, j + i):
                     temp = dp[j][k] + dp[k + 1][j + i]
                     if s[k] == s[i + j]:
                         temp -= 1
                     dp[j][j + i] = min(dp[j][j + i], temp)
-                    k += 1
-                j += 1
         return dp[0][size - 1]
 
 if __name__ == '__main__':
