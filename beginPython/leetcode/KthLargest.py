6fc42e2aa987ae6b669499808695c1cd3c193cec
ykliu <moneyflying_2006@hotmail.com>
2018-07-15 10:08:15 +0800

    learn python

---
+++ beginPython/leetcode/KthLargest.py
@@ -0,0 +1,38 @@
+import heapq
+
+
+class KthLargest(object):
+
+    def __init__(self, k, nums):
+        """
+        :type k: int
+        :type nums: List[int]
+        """
+        self.pool = nums
+        self.size = len(self.pool)
+        self.k = k
+        heapq.heapify(self.pool)
+        while self.size > k:
+            heapq.heappop(self.pool)
+            self.size -= 1
+
+    def add(self, val):
+        """
+        :type val: int
+        :rtype: int
+        """
+        if self.size < self.k:
+            heapq.heappush(self.pool, val)
+            self.size += 1
+        elif val > self.pool[0]:
+            heapq.heapreplace(self.pool, val)
+        return self.pool[0]
+
+if __name__ == '__main__':
+    a = KthLargest(3, [4, 5, 8, 2])
+    print(a.add(3))
+    print(a.add(5))
+    print(a.add(10))
+    print(a.add(8))
+    print(a.add(9))
+    print(a.add(4))
