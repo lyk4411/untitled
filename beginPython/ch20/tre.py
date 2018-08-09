5070e84ae5b0dd92d19f8199da38a7e06e5532a5
ykliu <moneyflying_2006@hotmail.com>
2018-08-09 16:20:24 +0800

    learn python

--- beginPython/ch20/tre.py
+++ beginPython/ch20/tre.py
@@ -15,5 +15,8 @@ def sub_mail(match):
     print("match.group(0): ",match.group(0))
     return '<a href="mailto:%s">%s</a>' % (match.group(1), match.group(1))
 
-# print(re.sub(r'\*(.+?)\*',sub_url,"*hello, world.*"))
+print(re.sub(r'\*(.+?)\*',sub_url,"*hello, world.*"))
 print(re.sub(r'\*(.+?)\*',sub_url,"hello, world."))
+
+text = "JGood isaandsomeboy, heiscool,clever,andoon..."
+print(re.sub(r'(\s+)', sub_emphasis, text))
