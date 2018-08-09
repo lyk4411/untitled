7f6c5c8e33fc11a2865f7c1bcbcb9cf7ea7720d5
ykliu <moneyflying_2006@hotmail.com>
2018-08-09 16:39:24 +0800

    learn python

--- beginPython/ch20/tre.py
+++ beginPython/ch20/tre.py
@@ -20,3 +20,19 @@ print(re.sub(r'\*(.+?)\*',sub_url,"hello, world."))
 
 text = "JGood isaandsomeboy, heiscool,clever,andoon..."
 print(re.sub(r'(\s+)', sub_emphasis, text))
+
+
+
+
+p = re.compile(r'(\w+) (\w+)')
+s = 'hello 123, hello 456'
+
+
+def func(m):
+    return 'hi' + ' ' + m.group(2)
+
+
+print (p.sub(r'hello world', s))  # 使用 'hello world' 替换 'hello 123' 和 'hello 456'
+print(p.sub(r'\2 \1', s)) # 引用分组
+print(p.sub(func, s))
+print(p.sub(func, s, 1))  # 最多替换一次
