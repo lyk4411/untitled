0ee3a63f644ba51d421d1f56150447d4888da8c7
ykliu <moneyflying_2006@hotmail.com>
2018-12-11 15:07:47 +0800

    learn python

--- beginPython/leetcode/ReorderLogFiles.py
+++ beginPython/leetcode/ReorderLogFiles.py
@@ -9,3 +9,5 @@ class ReorderLogFiles():
 if __name__ == '__main__':
     a = ReorderLogFiles()
     print(a.reorderLogFiles(["a1 9 2 3 1", "g1 act car", "zo4 4 7", "ab1 off key dog", "a8 act zoo"]))
+    b = [('a', 1, 'a'), ('a', 2, 'c'), ('b', 6, 'b'), ('b', 4, 'a'), ('a', 3, 'b')]
+    print(sorted(b,key = lambda x :(x[0], x[2])))
