93d5942ae614f3863242554c8c8afaa42c98160f
ykliu <moneyflying_2006@hotmail.com>
2018-12-11 14:41:58 +0800

    learn python

---
+++ beginPython/leetcode/DeleteColumnstoMakeSorted.py
@@ -0,0 +1,17 @@
+class  DeleteColumnstoMakeSorted():
+    def minDeletionSize(self, A):
+        """
+        :type A: List[str]
+        :rtype: int
+        """
+        ans = 0
+        for col in zip(*A):
+            if any(col[i] > col[i + 1] for i in range(len(col) - 1)):
+                ans += 1
+        return ans
+
+if __name__ == '__main__':
+    a = DeleteColumnstoMakeSorted()
+    print(a.minDeletionSize(["cba", "daf", "ghi"]))
+    print(a.minDeletionSize(["a", "b"]))
+    print(a.minDeletionSize( ["zyx", "wvu", "tsr"]))
