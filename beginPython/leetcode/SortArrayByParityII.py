29b7c51ed324a9edb773919a881ee8efb1888cb1
ykliu <moneyflying_2006@hotmail.com>
2018-12-20 19:06:12 +0800

    learn python

---
+++ beginPython/leetcode/SortArrayByParityII.py
@@ -0,0 +1,26 @@
+class SortArrayByParityII(object):
+    def sortArrayByParityII(self, A):
+        N = len(A)
+        ans = [None] * N
+
+        t = 0
+        for i, x in enumerate(A):
+            if x % 2 == 0:
+                ans[t] = x
+                t += 2
+
+        t = 1
+        for i, x in enumerate(A):
+            if x % 2 == 1:
+                ans[t] = x
+                t += 2
+
+        # We could have also used slice assignment:
+        # ans[::2] = (x for x in A if x % 2 == 0)
+        # ans[1::2] = (x for x in A if x % 2 == 1)
+
+        return ans
+
+if __name__ == '__main__':
+    a = SortArrayByParityII()
+    print(a.sortArrayByParityII([4, 2, 5, 7]))
