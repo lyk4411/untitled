bd763816050e3d63ab2ac7fa6e1b2879aa34c58c
ykliu <moneyflying_2006@hotmail.com>
2018-12-21 11:29:40 +0800

    learn python

--- beginPython/leetcode/LongPressedName.py
+++ beginPython/leetcode/LongPressedName.py
@@ -7,7 +7,10 @@ class LongPressedName(object):
         g2 = [(k, len(list(grp))) for k, grp in itertools.groupby(typed)]
         if len(g1) != len(g2):
             return False
-
+        # print(g1)
+        # print(g2)
+        # print(list(zip(g1, g2)))
+        # print(list(zip(g1)))
         return all(k1 == k2 and v1 <= v2
                    for (k1, v1), (k2, v2) in zip(g1, g2))
 
