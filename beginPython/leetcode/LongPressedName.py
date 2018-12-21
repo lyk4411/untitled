cab1fb5b45f184b1c9933eab07bef6bf4b3446b9
ykliu <moneyflying_2006@hotmail.com>
2018-12-21 10:56:48 +0800

    learn python

---
+++ beginPython/leetcode/LongPressedName.py
@@ -0,0 +1,18 @@
+import itertools
+
+
+class LongPressedName(object):
+    def isLongPressedName(self, name, typed):
+        g1 = [(k, len(list(grp))) for k, grp in itertools.groupby(name)]
+        g2 = [(k, len(list(grp))) for k, grp in itertools.groupby(typed)]
+        if len(g1) != len(g2):
+            return False
+
+        return all(k1 == k2 and v1 <= v2
+                   for (k1, v1), (k2, v2) in zip(g1, g2))
+
+if __name__ == '__main__':
+    a = LongPressedName()
+    print(a.isLongPressedName("alex", "aaleex"))
+    print(a.isLongPressedName("saeed", "ssaaed"))
+    print(a.isLongPressedName("leelee", "leelee"))
