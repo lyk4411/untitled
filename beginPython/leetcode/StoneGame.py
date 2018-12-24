24809baf2cdb59263f6128c1757078da1523f867
ykliu <moneyflying_2006@hotmail.com>
2018-12-24 16:44:21 +0800

    learn python

--- beginPython/leetcode/StoneGame.py
+++ beginPython/leetcode/StoneGame.py
@@ -1,19 +1,19 @@
 class StoneGame(object):
     def stoneGame(self, piles):
         N = len(piles)
-        dp = [[0 for _ in range(len(piles) + 1)] for _ in range(len(piles) + 1)]
+        dp = [[0 for _ in range(len(piles) + 2)] for _ in range(len(piles) + 2)]
 
         for size in range(1, len(piles) + 1):
             for i in range(0, len(piles) - size + 1):
                 j = i + size - 1
                 if ((j + i + N) % 2 == 1):
-                    dp[i][j] = max(piles[i] + dp[i + 1][j], piles[j] + dp[i][j - 1])
-                    print("dp[" + str(i) + "][" + str(j) + "]:" + str(dp[i][j]))
+                    dp[i + 1][j + 1] = max(piles[i] + dp[i + 2][j + 1], piles[j] + dp[i + 1][j])
+                    print("dp[" + str(i + 1) + "][" + str(j + 1) + "]:" + str(dp[i + 1][j + 1]))
                 else:
-                    dp[i][j] = min(-piles[i] + dp[i + 1][j], -piles[j] + dp[i][j - 1])
-                    print("dp[" + str(i) + "][" + str(j) + "]:" + str(dp[i][j]))
+                    dp[i + 1][j + 1] = min(-piles[i] + dp[i + 2][j + 1], -piles[j] + dp[i + 1][j])
+                    print("dp[" + str(i + 1) + "][" + str(j + 1) + "]:" + str(dp[i + 1][j + 1]))
 
-        return dp[0][len(piles) - 1] > 0
+        return dp[1][len(piles)] > 0
 
 if __name__ == '__main__':
     a = StoneGame()
