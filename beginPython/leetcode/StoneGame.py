0b529de891d831db913c8dd617f2a47433124d38
ykliu <moneyflying_2006@hotmail.com>
2018-12-24 15:29:11 +0800

    learn python

--- beginPython/leetcode/StoneGame.py
+++ beginPython/leetcode/StoneGame.py
@@ -1,17 +1,19 @@
 class StoneGame(object):
     def stoneGame(self, piles):
         N = len(piles)
+        dp = [[0 for _ in range(len(piles) + 1)] for _ in range(len(piles) + 1)]
 
-        def dp(i, j):
-            # The value of the game [piles[i], piles[i+1], ..., piles[j]].
-            if i > j: return 0
-            parity = (j - i + N) % 2
-            if parity == 1:  # first player
-                return max(piles[i] + dp(i + 1, j), piles[j] + dp(i, j - 1))
-            else:
-                return min(-piles[i] + dp(i + 1, j), -piles[j] + dp(i, j - 1))
+        for size in range(1, len(piles) + 1):
+            for i in range(0, len(piles) - size + 1):
+                j = i + size - 1
+                if ((j + i + N) % 2 == 1):
+                    dp[i][j] = max(piles[i] + dp[i + 1][j], piles[j] + dp[i][j - 1])
+                    print("dp[" + str(i) + "][" + str(j) + "]:" + str(dp[i][j]))
+                else:
+                    dp[i][j] = min(-piles[i] + dp[i + 1][j], -piles[j] + dp[i][j - 1])
+                    print("dp[" + str(i) + "][" + str(j) + "]:" + str(dp[i][j]))
 
-        return dp(0, N - 1) > 0
+        return dp[0][len(piles) - 1] >= 0
 
 if __name__ == '__main__':
     a = StoneGame()
