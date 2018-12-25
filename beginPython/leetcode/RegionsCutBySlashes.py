974e8a948ad278f001603058eda8bd4b07e65973
ykliu <moneyflying_2006@hotmail.com>
2018-12-25 18:00:13 +0800

    learn python

--- beginPython/leetcode/RegionsCutBySlashes.py
+++ beginPython/leetcode/RegionsCutBySlashes.py
@@ -1,18 +1,15 @@
 class DSU:
     def __init__(self, N):
         self.p = list(range(N))
-
-
     def find(self, x):
         if self.p[x] != x:
             self.p[x] = self.find(self.p[x])
         return self.p[x]
-
-
     def union(self, x, y):
         xr = self.find(x)
         yr = self.find(y)
         self.p[xr] = yr
+
 class RegionsCutBySlashes(object):
     def regionsBySlashes(self, grid):
         N = len(grid)
