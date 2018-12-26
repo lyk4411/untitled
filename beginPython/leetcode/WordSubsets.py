fc643433a9ddb8d7e0fd5443d7826efacf0819b8
ykliu <moneyflying_2006@hotmail.com>
2018-12-26 16:33:02 +0800

    learn python

---
+++ beginPython/leetcode/WordSubsets.py
@@ -0,0 +1,22 @@
+class WordSubsets(object):
+    def wordSubsets(self, A, B):
+        def count(word):
+            ans = [0] * 26
+            for letter in word:
+                ans[ord(letter) - ord('a')] += 1
+            return ans
+
+        bmax = [0] * 26
+        for b in B:
+            for i, c in enumerate(count(b)):
+                bmax[i] = max(bmax[i], c)
+
+        ans = []
+        for a in A:
+            if all(x >= y for x, y in zip(count(a), bmax)):
+                ans.append(a)
+        return ans
+
+if __name__ == '__main__':
+    a = WordSubsets()
+    print(a.wordSubsets(["amazon","apple","facebook","google","leetcode"], ["e","oo"]))
