ee00daf8e7e1bf928982079f83a016fe78a75775
ykliu <moneyflying_2006@hotmail.com>
2018-12-28 20:16:26 +0800

    learn python

--- beginPython/leetcode/kthSmallestPrimeFraction.py
+++ beginPython/leetcode/kthSmallestPrimeFraction.py
@@ -5,7 +5,8 @@ class kthSmallestPrimeFraction(object):
     def kthSmallestPrimeFraction(self, A, K):
         l = len(A) - 1
         h = []
-        for i in range(l):  # 先往把每个元素的最小值加入heapq中，heapq中的每个元素第一位表示p/q的值，第二个表示p在A中的索引，第三位表示q在A中的索引
+        for i in range(l):  # 先往把每个元素的最小值加入heapq中，heapq中的每个元素第一位表示p/q的值，
+            # 第二个表示p在A中的索引，第三位表示q在A中的索引
             heapq.heappush(h, (A[i] / float(A[l]), i, l)) # 一个个查找
         while K > 0: # 输出最小的一个
             min_one = heapq.heappop(h) # 添加保持min_one的p不变，q选择的索引 + 1，并且保证p<q
