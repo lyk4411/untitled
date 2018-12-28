d450e9a2613d86cc33a208b86ebe7e427d21d993
ykliu <moneyflying_2006@hotmail.com>
2018-12-28 20:56:20 +0800

    learn python

---
+++ beginPython/leetcode/BusRoutes.py
@@ -0,0 +1,32 @@
+import collections
+
+
+class BusRoutes(object):
+    def numBusesToDestination(self, routes, S, T):
+        if S == T: return 0
+        N = len(routes)
+        graph = collections.defaultdict(set)
+        for i, r1 in enumerate(routes):
+            for j in range(i + 1, N):
+                r2 = routes[j]
+                if any(r in r2 for r in r1):
+                    graph[i].add(j)
+                    graph[j].add(i)
+
+        seen, targets = set(), set()
+        for node, route in enumerate(routes):
+            if S in route: seen.add(node)
+            if T in route: targets.add(node)
+
+        queue = [(node, 1) for node in seen]
+        for node, depth in queue:
+            if node in targets: return depth
+            for nei in graph[node]:
+                if nei not in seen:
+                    seen.add(nei)
+                    queue.append((nei, depth + 1))
+        return -1
+
+if __name__ == '__main__':
+    a = BusRoutes()
+    print(a.numBusesToDestination([[1, 2, 7], [3, 6, 7]], 1, 6))
