c00c321100fb8df7c901528a72a047662306b7c2
ykliu <moneyflying_2006@hotmail.com>
2018-12-30 10:18:57 +0800

    learn python

---
+++ beginPython/leetcode/SumofDistancesinTree.py
@@ -0,0 +1,33 @@
+import collections
+
+
+class SumofDistancesinTree(object):
+    def sumOfDistancesInTree(self, N, edges):
+        graph = collections.defaultdict(set)
+        for u, v in edges:
+            graph[u].add(v)
+            graph[v].add(u)
+
+        count = [1] * N
+        ans = [0] * N
+
+        def dfs(node=0, parent=None):
+            for child in graph[node]:
+                if child != parent:
+                    dfs(child, node)
+                    count[node] += count[child]
+                    ans[node] += ans[child] + count[child]
+
+        def dfs2(node=0, parent=None):
+            for child in graph[node]:
+                if child != parent:
+                    ans[child] = ans[node] - count[child] + N - count[child]
+                    dfs2(child, node)
+
+        dfs()
+        dfs2()
+        return ans
+
+if __name__ == '__main__':
+    a = SumofDistancesinTree()
+    print(a.sumOfDistancesInTree(6, [[0, 1], [0, 2], [2, 3], [2, 4], [2, 5]]))
