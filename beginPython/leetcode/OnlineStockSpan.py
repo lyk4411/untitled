bb22b2e99d018e93085630b6395e2c0a4f1b32ac
ykliu <moneyflying_2006@hotmail.com>
2019-02-08 19:22:27 +0800

    learn python

---
+++ beginPython/leetcode/OnlineStockSpan.py
@@ -0,0 +1,21 @@
+class OnlineStockSpan(object):
+    def __init__(self):
+        self.stack = []
+
+    def next(self, price):
+        weight = 1
+        while self.stack and self.stack[-1][0] <= price:
+            weight += self.stack.pop()[1]
+        self.stack.append((price, weight))
+        return weight
+
+if __name__ == '__main__':
+    a = OnlineStockSpan()
+    print(a.next(100))
+    print(a.next(80))
+    print(a.next(60))
+    print(a.next(70))
+    print(a.next(60))
+    print(a.next(75))
+    print(a.next(85))
+    print(a.next(90))
