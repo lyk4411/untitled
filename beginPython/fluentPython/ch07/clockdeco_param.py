a0ee718747c09801557047108a8a65a4797e982c
ykliu <moneyflying_2006@hotmail.com>
2019-04-10 19:27:35 +0800

    learn python

--- beginPython/fluentPython/ch07/clockdeco_param.py
+++ beginPython/fluentPython/ch07/clockdeco_param.py
@@ -1,15 +1,14 @@
 # clockdeco_param.py
+#
+# """
+# >>> snooze(.1)  # doctest: +ELLIPSIS
+# [0.101...s] snooze(0.1) -> None
+# >>> clock('{name}: {elapsed}')(time.sleep)(.2)  # doctest: +ELLIPSIS
+# sleep: 0.20...
+# >>> clock('{name}({args}) dt={elapsed:0.3f}s')(time.sleep)(.2)
+# sleep(0.2) dt=0.201s
+# """
 
-"""
->>> snooze(.1)  # doctest: +ELLIPSIS
-[0.101...s] snooze(0.1) -> None
->>> clock('{name}: {elapsed}')(time.sleep)(.2)  # doctest: +ELLIPSIS
-sleep: 0.20...
->>> clock('{name}({args}) dt={elapsed:0.3f}s')(time.sleep)(.2)
-sleep(0.2) dt=0.201s
-"""
-
-# BEGIN CLOCKDECO_PARAM
 import time
 
 DEFAULT_FMT = '[{elapsed:0.8f}s] {name}({args}) -> {result}'
@@ -23,6 +22,7 @@ def clock(fmt=DEFAULT_FMT):  # <1>
             name = func.__name__
             args = ', '.join(repr(arg) for arg in _args)  # <5>
             result = repr(_result)  # <6>
+            # print(locals())
             print(fmt.format(**locals()))  # <7>
             return _result  # <8>
         return clocked  # <9>
@@ -37,4 +37,4 @@ if __name__ == '__main__':
     for i in range(3):
         snooze(.123)
 
-# END CLOCKDECO_PARAM
+
