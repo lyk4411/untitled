7443667355212c7fac6bebfa8aa8b48ab473e868
ykliu <moneyflying_2006@hotmail.com>
2019-04-19 14:02:28 +0800

    learn python

---
+++ beginPython/fluentPython/ch17/flattenyield.py
@@ -0,0 +1,17 @@
+import collections
+
+
+def flatten(items, ingore_types=(str, bytes)):
+    for x in items:
+        # isinstance(x, collections.Iterable) 检查是否有某个元素是可迭代的；
+        # 如果有，那么就用yield from将这个可迭代对象作为一种子例程进行递归，它将所有的值都产生出来
+        if isinstance(x, collections.Iterable) and not isinstance(x, ingore_types):
+            # not isinstance(x, ingore_types)是为了避免将字符串和字节串解释为可迭代对象，进而将他们展开为单独的一个个字符
+            yield from flatten(x)
+        else:
+            yield x
+
+
+items = [1, 2, [3, 4, [5, 6], 7], 8]
+for x in flatten(items):
+    print(x)
