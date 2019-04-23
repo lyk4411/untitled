7100cb6bc86b1a832c7a05f3d241287b679f3105
ykliu <moneyflying_2006@hotmail.com>
2019-04-23 10:04:57 +0800

    learn python

---
+++ beginPython/fluentPython/ch19/osconfeed.py
@@ -0,0 +1,48 @@
+#     >>> feed = load()  # <1>
+#     >>> sorted(feed['Schedule'].keys())  # <2>
+#     ['conferences', 'events', 'speakers', 'venues']
+#     >>> for key, value in sorted(feed['Schedule'].items()):
+#     ...     print('{:3} {}'.format(len(value), key))  # <3>
+#     ...
+#       1 conferences
+#     484 events
+#     357 speakers
+#      53 venues
+#     >>> feed['Schedule']['speakers'][-1]['name']  # <4>
+#     'Carina C. Zona'
+#     >>> feed['Schedule']['speakers'][-1]['serial']  # <5>
+#     141590
+#     >>> feed['Schedule']['events'][40]['name']
+#     'There *Will* Be Bugs'
+#     >>> feed['Schedule']['events'][40]['speakers']  # <6>
+#     [3471, 5199]
+#
+#
+
+from urllib.request import urlopen
+import warnings
+import os
+import json
+
+URL = 'http://www.oreilly.com/pub/sc/osconfeed'
+JSON = 'data/osconfeed.json'
+
+
+def load():
+    if not os.path.exists(JSON):
+        msg = 'downloading {} to {}'.format(URL, JSON)
+        warnings.warn(msg)  # <1>
+        with urlopen(URL) as remote, open(JSON, 'wb') as local:  # <2>
+            local.write(remote.read())
+
+    with open(JSON) as fp:
+        return json.load(fp)  # <3>
+
+if __name__ == '__main__':
+    load()
+    print("=====================================================")
+    feed = load()
+    print(sorted(feed['Schedule'].keys()))
+    for key, value in sorted(feed['Schedule'].items()):
+         print('{:3} {}'.format(len(value), key))  # <3>
+    print(feed['Schedule']['speakers'][-1]['name'])
