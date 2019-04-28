b73c3f921952efd8066502ce421df49807b9082f
ykliu <moneyflying_2006@hotmail.com>
2019-04-28 15:13:08 +0800

    learn python

--- beginPython/cookbook/2/searching_and_replacing_text/example.py
+++ beginPython/cookbook/2/searching_and_replacing_text/example.py
@@ -8,9 +8,9 @@ import re
 text = 'Today is 11/27/2012. PyCon starts 3/13/2013.'
 
 datepat = re.compile(r'(\d+)/(\d+)/(\d+)')
-
-# (a) Simple substitution
-print(datepat.sub(r'\3-\1-\2', text))
+#
+# # (a) Simple substitution
+# print(datepat.sub(r'\3-\1-\2', text))
 
 # (b) Replacement function
 from calendar import month_abbr
