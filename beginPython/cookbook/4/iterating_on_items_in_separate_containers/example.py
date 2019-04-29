fb3170fe9b9cf3f076235befae64f54340b5336b
ykliu <moneyflying_2006@hotmail.com>
2019-04-29 14:50:04 +0800

    learn python

--- beginPython/cookbook/4/iterating_on_items_in_separate_containers/example.py
+++ beginPython/cookbook/4/iterating_on_items_in_separate_containers/example.py
@@ -6,3 +6,17 @@ b = ['x', 'y', 'z']
 for x in chain(a, b):
     print(x)
 
+print("=============================")
+c = set()
+d = set()
+c.add(1)
+c.add(2)
+c.add(3)
+c.add(4)
+d.add(1)
+d.add(2)
+d.add(3)
+d.add(4)
+d.add(5)
+for x in chain(c, d):
+    print(x)
