f8c3faa8346a5b236119628ec7741e254ea0106e
ykliu <moneyflying_2006@hotmail.com>
2019-04-29 18:48:12 +0800

    learn python

---
+++ beginPython/cookbook/5/getfilenames.py
@@ -0,0 +1,14 @@
+
+
+import os
+names = os.listdir('.')
+print(names)
+
+names1 = [name for name in os.listdir('getting_a_directory_listing')
+          if os.path.isfile(os.path.join('getting_a_directory_listing',name))]
+print(names1)
+
+names2 = [name for name in os.listdir('.')
+          if os.path.isfile(os.path.join('.',name))]
+print(names2)
+
