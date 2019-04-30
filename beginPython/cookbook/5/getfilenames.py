6f3048f9db37308028e4dcc5482b2891aa2de080
ykliu <moneyflying_2006@hotmail.com>
2019-04-30 13:44:58 +0800

    learn python

--- beginPython/cookbook/5/getfilenames.py
+++ beginPython/cookbook/5/getfilenames.py
@@ -3,28 +3,28 @@
 import os
 names = os.listdir('.')
 print(names)
-
+print("================================================================")
 names1 = [name for name in os.listdir('getting_a_directory_listing')
           if os.path.isfile(os.path.join('getting_a_directory_listing',name))]
 print(names1)
-
+print("================================================================")
 names2 = [name for name in os.listdir('.')
           if os.path.isfile(os.path.join('.',name))]
 print(names2)
-
+print("================================================================")
 import sys
 print(sys.getfilesystemencoding())
-
+print("================================================================")
 
 print(os.listdir('.'))
 print(os.listdir(b'.'))
 
-
+print("================================================================")
 def bad_filename(filename):
     return repr(filename)[1:-1]
 
 
-print("=====================================")
+print("================================================================")
 for name in os.listdir('.'):
     try:
         print(bad_filename(name))
