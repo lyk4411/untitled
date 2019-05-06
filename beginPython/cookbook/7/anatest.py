ec5211285ae4f7ecc33c730cc810a97a9e93296c
ykliu <moneyflying_2006@hotmail.com>
2019-05-06 16:38:08 +0800

    learn python

---
+++ beginPython/cookbook/7/anatest.py
@@ -0,0 +1,7 @@
+names = ['abalajfdlan','hello world','wodemaya','nihao']
+
+print(sorted(names,key = lambda name: name.split()[-1].lower()))
+
+print('abalajfdlan'.split())
+print('hello world'.split())
+print('hello world'.split()[-1])
