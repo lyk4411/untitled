5e65e3f6a1b7e06688f4ce972888393d4cc249e7
ykliu <moneyflying_2006@hotmail.com>
2019-05-06 10:27:59 +0800

    learn python

--- beginPython/cookbook/15/coroutinestest.py
+++ beginPython/cookbook/15/coroutinestest.py
@@ -1,3 +1,6 @@
+from queue import Queue
+
+
 class Task(object):
     taskid = 0
     def __init__(self,target):
@@ -20,3 +23,35 @@ t1 = Task(foo())
 
 t1.run()
 t1.run()
+
+
+class Scheduler(object):
+    def __init__(self):
+        self.ready = Queue()
+        self.taskmap = {}
+    def new(self,target):
+        newtask = Task(target)
+        self.taskmap[newtask.tid] = newtask
+        self.schedule(newtask)
+        return newtask.tid
+    def schedule(self,task):
+       self.ready.put(task)
+    def mainloop(self):
+        while self.taskmap:
+            task = self.ready.get()
+            result = task.run()
+            self.schedule(task)
+
+def foo():
+    while True:
+        print ("I'm foo")
+        yield
+def bar():
+    while True:
+        print ("I'm bar")
+        yield
+
+sched = Scheduler()
+sched.new(foo())
+sched.new(bar())
+sched.mainloop()
