9baf09ad7e84b7eecf1d6a328fd6c8bea679c8aa
ykliu <moneyflying_2006@hotmail.com>
2019-05-06 16:48:42 +0800

    learn python

--- beginPython/cookbook/7/anatest.py
+++ beginPython/cookbook/7/anatest.py
@@ -5,3 +5,12 @@ print(sorted(names,key = lambda name: name.split()[-1].lower()))
 print('abalajfdlan'.split())
 print('hello world'.split())
 print('hello world'.split()[-1])
+
+funcs = [lambda x : x + n for n in range(5)]
+for f in funcs:
+    print(f(0))
+
+print("===========================================")
+funcs = [lambda x, n = n : x + n for n in range(5)]
+for f in funcs:
+    print(f(0))
