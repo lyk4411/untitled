27f473b34542a4dd5f1a960d29cc2a25962fd98a
ykliu <moneyflying_2006@hotmail.com>
2019-05-14 15:21:49 +0800

    learn python

--- beginPython/cookbook/10/loading_modules_from_a_remote_machine_using_import_hooks/pathexample.py
+++ beginPython/cookbook/10/loading_modules_from_a_remote_machine_using_import_hooks/pathexample.py
@@ -5,14 +5,14 @@ if False:
     import logging
     logging.basicConfig(level=logging.DEBUG)
 
-import urlimport
+from . import urlimport
 urlimport.install_path_hook()
 
 import sys
 sys.path.append('http://localhost:15000')
 
-import fib
-import spam
-import grok.blah
-print(grok.blah.__file__)
+from .testcode import fib
+from .testcode import spam
+from .testcode.grok import blah
+print(blah.__file__)
 
