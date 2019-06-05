a4774378be6d94856307d6e1b85bdf2c19fc265b
ykliu <moneyflying_2006@hotmail.com>
2019-06-05 15:30:40 +0800

    learn python

--- beginPython/programmingCollectiveIntelligence/chapter10/learnlist.py
+++ beginPython/programmingCollectiveIntelligence/chapter10/learnlist.py
@@ -3,4 +3,4 @@
 aa = [{'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6},{'a':1, 'b':2,  'd':4, 'f':6},
       {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6},{'a':1, 'b':2,  'd':4, 'e':5, 'f':6}]
 vec = ['a','c','e','f']
-print([[(word in f and f[word]or 0) for word in vec] for f in aa])
+print([[(word in f and f[word] or 0) for word in vec] for f in aa])
