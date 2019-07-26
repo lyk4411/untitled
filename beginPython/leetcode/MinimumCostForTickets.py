834e331a6352347423d4e9dcf7fb8c5f69a13955
ykliu <moneyflying_2006@hotmail.com>
2019-07-26 11:27:04 +0800

    learn python

--- beginPython/leetcode/MinimumCostForTickets.py
+++ beginPython/leetcode/MinimumCostForTickets.py
@@ -1,24 +1,37 @@
 from functools import lru_cache
 
 class MinimumCostForTickets(object):
+    # def mincostTickets(self, days, costs):
+    #     N = len(days)
+    #     durations = [1, 7, 30]
+    #
+    #     @lru_cache(None)
+    #     def dp(i):  # How much money to do days[i]+
+    #         if i >= N: return 0
+    #
+    #         ans = float('inf')
+    #         j = i
+    #         for c, d in zip(costs, durations):
+    #             while j < N and days[j] < days[i] + d:
+    #                 j += 1
+    #             ans = min(ans, dp(j) + c)
+    #
+    #         return ans
+    #
+    #     return dp(0)
+
+
+
+
     def mincostTickets(self, days, costs):
-        N = len(days)
-        durations = [1, 7, 30]
-
-        @lru_cache(None)
-        def dp(i):  # How much money to do days[i]+
-            if i >= N: return 0
-
-            ans = float('inf')
-            j = i
-            for c, d in zip(costs, durations):
-                while j < N and days[j] < days[i] + d:
-                    j += 1
-                ans = min(ans, dp(j) + c)
-
-            return ans
-
-        return dp(0)
+        dp = [0] * 366
+        days_set = set(days)  # 变成集合，后面查找的时间复杂度为 O(1)
+        for i in range(1, days[-1] + 1):
+            if i not in days_set:
+                dp[i] = dp[i - 1]
+            else:
+                dp[i] = min(dp[i - 1] + costs[0], dp[i - 7] + costs[1], dp[i - 30] + costs[2])
+        return dp[days[-1]]
 
 if __name__ == '__main__':
     a = MinimumCostForTickets()
