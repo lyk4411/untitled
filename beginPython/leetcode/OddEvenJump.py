5b0b318e9dc80c42da49b2772b27f4965ba91f37
ykliu <moneyflying_2006@hotmail.com>
2019-08-17 22:35:55 +0800

    learn python

--- beginPython/leetcode/OddEvenJump.py
+++ beginPython/leetcode/OddEvenJump.py
@@ -1,32 +1,26 @@
 class OddEvenJump(object):
     def oddEvenJumps(self, A):
-        N = len(A)
+        n = len(A)
+        next_higher, next_lower = [0] * n, [0] * n
 
-        def make(B):
-            ans = [None] * N
-            stack = []  # invariant: stack is decreasing
-            for i in B:
-                while stack and i > stack[-1]:
-                    ans[stack.pop()] = i
-                stack.append(i)
-            return ans
+        stack = []
+        for a, i in sorted([a, i] for i, a in enumerate(A)):
+            while stack and stack[-1] < i:
+                next_higher[stack.pop()] = i
+            stack.append(i)
 
-        B = sorted(range(N), key=lambda i: A[i])
-        oddnext = make(B)
-        B.sort(key=lambda i: -A[i])
-        evennext = make(B)
+        stack = []
+        for a, i in sorted([-a, i] for i, a in enumerate(A)):
+            while stack and stack[-1] < i:
+                next_lower[stack.pop()] = i
+            stack.append(i)
 
-        odd = [False] * N
-        even = [False] * N
-        odd[N - 1] = even[N - 1] = True
-
-        for i in range(N - 2, -1, -1):
-            if oddnext[i] is not None:
-                odd[i] = even[oddnext[i]]
-            if evennext[i] is not None:
-                even[i] = odd[evennext[i]]
-
-        return sum(odd)
+        higher, lower = [0] * n, [0] * n
+        higher[-1] = lower[-1] = 1
+        for i in range(n - 1)[::-1]:
+            higher[i] = lower[next_higher[i]]
+            lower[i] = higher[next_lower[i]]
+        return sum(higher)
 
 if __name__ == '__main__':
     a = OddEvenJump()
