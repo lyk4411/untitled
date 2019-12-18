7eca123731d59768df703841636d533db2b4d839
ykliu <moneyflying_2006@hotmail.com>
2019-12-18 16:21:20 +0800

    learn python

---
+++ effectivePython/item_39a.py
@@ -0,0 +1,118 @@
+
+# Reproduce book environment
+import random
+
+import logging
+from pprint import pprint
+from sys import stdout as STDOUT
+
+# Write all output to a temporary directory
+import atexit
+import gc
+import io
+import os
+import tempfile
+
+
+def close_open_files():
+    everything = gc.get_objects()
+    for obj in everything:
+        if isinstance(obj, io.IOBase):
+            obj.close()
+
+atexit.register(close_open_files)
+
+
+# Example 1
+class InputData:
+    def read(self):
+        raise NotImplementedError
+
+
+# Example 2
+class PathInputData(InputData):
+    def __init__(self, path):
+        super().__init__()
+        self.path = path
+
+    def read(self):
+        with open(self.path) as f:
+            return f.read()
+
+
+# Example 3
+class Worker:
+    def __init__(self, input_data):
+        self.input_data = input_data
+        self.result = None
+
+    def map(self):
+        raise NotImplementedError
+
+    def reduce(self, other):
+        raise NotImplementedError
+
+
+# Example 4
+class LineCountWorker(Worker):
+    def map(self):
+        data = self.input_data.read()
+        self.result = data.count('\n')
+
+    def reduce(self, other):
+        self.result += other.result
+
+
+# Example 5
+import os
+
+def generate_inputs(data_dir):
+    for name in os.listdir(data_dir):
+        yield PathInputData(os.path.join(data_dir, name))
+
+
+# Example 6
+def create_workers(input_list):
+    workers = []
+    for input_data in input_list:
+        workers.append(LineCountWorker(input_data))
+    return workers
+
+
+# Example 7
+from threading import Thread
+
+def execute(workers):
+    threads = [Thread(target=w.map) for w in workers]
+    for thread in threads: thread.start()
+    for thread in threads: thread.join()
+
+    first, *rest = workers
+    for worker in rest:
+        first.reduce(worker)
+    return first.result
+
+
+# Example 8
+def mapreduce(data_dir):
+    inputs = generate_inputs(data_dir)
+    workers = create_workers(inputs)
+    return execute(workers)
+
+
+# Example 9
+import os
+import random
+
+def write_test_files(tmpdir):
+    os.makedirs(tmpdir)
+    for i in range(100):
+        with open(os.path.join(tmpdir, str(i)), 'w') as f:
+            f.write('a\n' * random.randint(0, 100))
+
+tmpdir = 'test_inputs'
+write_test_files(tmpdir)
+
+if __name__ == '__main__':
+    result = mapreduce(tmpdir)
+    print(f'There are {result} lines')
