9860f20586137442ef34e3cd130ef34eb3c3a5d5
ykliu <moneyflying_2006@hotmail.com>
2019-12-23 10:34:26 +0800

    learn python

---
+++ effectivePython/item_54a.py
@@ -0,0 +1,77 @@
+
+# Reproduce book environment
+import random
+random.seed(1234)
+
+import logging
+from pprint import pprint
+from sys import stdout as STDOUT
+
+# Write all output to a temporary directory
+import atexit
+import gc
+import io
+import os
+import tempfile
+
+TEST_DIR = tempfile.TemporaryDirectory()
+atexit.register(TEST_DIR.cleanup)
+
+# Make sure Windows processes exit cleanly
+OLD_CWD = os.getcwd()
+atexit.register(lambda: os.chdir(OLD_CWD))
+os.chdir(TEST_DIR.name)
+
+def close_open_files():
+    everything = gc.get_objects()
+    for obj in everything:
+        if isinstance(obj, io.IOBase):
+            obj.close()
+
+atexit.register(close_open_files)
+
+
+# Example 1
+class Counter:
+    def __init__(self):
+        self.count = 0
+
+    def increment(self, offset):
+        self.count += offset
+
+
+# Example 2
+def worker(sensor_index, how_many, counter):
+    # I have a barrier in here so the workers synchronize
+    # when they start counting, otherwise it's hard to get a race
+    # because the overhead of starting a thread is high.
+    BARRIER.wait()
+    for _ in range(how_many):
+        # Read from the sensor
+        # Nothing actually happens here, but this is where
+        # the blocking I/O would go.
+        counter.increment(1)
+
+
+# Example 3
+from threading import Barrier
+BARRIER = Barrier(5)
+from threading import Thread
+
+how_many = 10**5
+counter = Counter()
+
+threads = []
+for i in range(5):
+    thread = Thread(target=worker,
+                    args=(i, how_many, counter))
+    threads.append(thread)
+    thread.start()
+
+for thread in threads:
+    thread.join()
+
+expected = how_many * 5
+found = counter.count
+print(f'Counter should be {expected}, got {found}')
+
