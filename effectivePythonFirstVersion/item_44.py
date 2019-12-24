b9ac712fac6e9ec92c3d7405151f5eee2ed31100
ykliu <moneyflying_2006@hotmail.com>
2019-12-24 16:47:25 +0800

    learn python

--- effectivePythonFirstVersion/item_44.py
+++ effectivePythonFirstVersion/item_44.py
@@ -67,7 +67,7 @@ print("aaaaaaa:",state_after.__dict__)
 # Example 7
 with open(state_path, 'rb') as f:
     state_after = pickle.load(f)
-print(state_after.__dict__)
+print("bbbbbbbbbbb:", state_after.__dict__)
 
 
 # Example 8
@@ -92,33 +92,35 @@ def pickle_game_state(game_state):
 def unpickle_game_state(kwargs):
     return GameState(**kwargs)
 
-#
-# # Example 12
-# import copyreg
-# copyreg.pickle(GameState, pickle_game_state)
-#
-#
-# # Example 13
-# state = GameState()
-# state.points += 1000
-# serialized = pickle.dumps(state)
-# state_after = pickle.loads(serialized)
-# print(state_after.__dict__)
-#
-#
-# # Example 14
-# class GameState(object):
-#     def __init__(self, level=0, lives=4, points=0, magic=5):
-#         self.level = level
-#         self.lives = lives
-#         self.points = points
-#         self.magic = magic
-#
-#
-# # Example 15
-# state_after = pickle.loads(serialized)
-# print(state_after.__dict__)
-#
+
+# Example 12
+import copyreg
+copyreg.pickle(GameState, pickle_game_state)
+
+print("########################### Example 13 ############################################")
+
+# Example 13
+state = GameState()
+state.points += 1000
+serialized = pickle.dumps(state)
+state_after = pickle.loads(serialized)
+print(state_after.__dict__)
+
+
+# Example 14
+class GameState(object):
+    def __init__(self, level=0, lives=4, points=0, magic=5):
+        self.level = level
+        self.lives = lives
+        self.points = points
+        self.magic = magic
+
+print("########################### Example 15 ############################################")
+
+# Example 15
+state_after = pickle.loads(serialized)
+print(state_after.__dict__)
+
 #
 # # Example 16
 # class GameState(object):
