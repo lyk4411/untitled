1f0070994e0b6895eb00586dfd32e756ca899a0b
ykliu <moneyflying_2006@hotmail.com>
2019-12-30 15:15:54 +0800

    learn python

--- pythonWebCrawler/cookieLearn.py
+++ pythonWebCrawler/cookieLearn.py
@@ -6,3 +6,10 @@ opener = urllib.request.build_opener(handle)
 response = opener.open('http://www.baidu.com')
 for item in cookie:
     print(item.name + " = " + item.value)
+
+
+from urllib.parse import quote,unquote
+keyword = '你好'
+url = 'http://www.baidu.com/s?wd=' + quote(keyword)
+print(url)
+print(unquote('http://www.baidu.com/s?wd=%E4%BD%A0%E5%A5%BD'))
