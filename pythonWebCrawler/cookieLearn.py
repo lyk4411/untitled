9de4b8f9209f9141314f1ecf13bf7d9358ab1550
ykliu <moneyflying_2006@hotmail.com>
2019-12-30 15:58:11 +0800

    learn python

--- pythonWebCrawler/cookieLearn.py
+++ pythonWebCrawler/cookieLearn.py
@@ -21,3 +21,13 @@ print(type(r))
 print(r.status_code)
 print(r.text)
 print(r.cookies)
+
+
+r = requests.get('https://github.com/favicon.ico')
+print(r.text)
+print('=============================')
+print(r.content)
+#
+# with open('favicon.ico', 'wb') as f:
+#     f.write(r.content)
+#
