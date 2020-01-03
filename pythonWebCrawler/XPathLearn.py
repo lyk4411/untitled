dd4331d1596c2d2dc4d33ea5f84f6de95e6a4cd5
ykliu <moneyflying_2006@hotmail.com>
2020-01-03 11:11:02 +0800

    learn python

--- pythonWebCrawler/XPathLearn.py
+++ pythonWebCrawler/XPathLearn.py
@@ -19,3 +19,65 @@ from bs4 import BeautifulSoup
 soup = BeautifulSoup(html, 'lxml')
 print(soup.prettify())
 print(soup.title.string)
+
+
+print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+html = """
+<html><head><title>The Dormouse's story</title></head>
+<body>
+<p class="title" name="dromouse"><b>The Dormouse's story</b></p>
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1"><!-- Elsie --></a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+<p class="story">...</p>
+"""
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(html, 'lxml')
+print(soup.title)
+print(type(soup.title))
+print(soup.title.string)
+print(soup.head)
+print(soup.p)
+
+print(soup.p.attrs)
+
+print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+html = """
+<html><head><title>The Dormouse's story</title></head>
+<body>
+"""
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(html, 'lxml')
+print(soup.head.title)
+print(type(soup.head.title))
+print(soup.head.title.string)
+
+print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+html = """
+<html>
+    <head>
+        <title>The Dormouse's story</title>
+    </head>
+    <body>
+        <p class="story">
+            Once upon a time there were three little sisters; and their names were
+            <a href="http://example.com/elsie" class="sister" id="link1">
+                <span>Elsie</span>
+            </a>
+            <a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> 
+            and
+            <a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>
+            and they lived at the bottom of a well.
+        </p>
+        <p class="story">...</p>
+"""
+
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(html, 'lxml')
+print(soup.p.children)
+for i, child in enumerate(soup.p.children):
+    print(i, child)
