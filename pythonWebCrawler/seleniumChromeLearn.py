3e640be7e811f896d94d5c8c6a1f95a70331c831
ykliu <moneyflying_2006@hotmail.com>
2020-01-07 10:13:12 +0800

    learn python

--- pythonWebCrawler/seleniumChromeLearn.py
+++ pythonWebCrawler/seleniumChromeLearn.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-driver = webdriver.Chrome(r'F:\software\chromedriver_win32')
+driver = webdriver.Chrome()
 driver.get('http://www.baidu.com')
 print (driver.title)
 driver.quit()
