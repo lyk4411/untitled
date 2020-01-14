ce3947fc3217a82634c40e90de1e345dbc383a6b
ykliu <moneyflying_2006@hotmail.com>
2020-01-14 11:28:53 +0800

    learn python

---
+++ ScrapyTutorial-master/entrypoint.py
@@ -0,0 +1,6 @@
+from scrapy import cmdline
+cmdline.execute(['scrapy', 'crawl', 'quotes'])
+
+
+print("===============================================================================================")
+# cmdline.execute(['scrapy', 'runspider', 'scrapydownloadertest/spiders/httpbin.py'])
