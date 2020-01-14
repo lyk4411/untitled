18e577ec45cf601004beca8583b362fefaf1464b
ykliu <moneyflying_2006@hotmail.com>
2020-01-14 15:11:00 +0800

    learn python

---
+++ Images360-master/entrypoint.py
@@ -0,0 +1,6 @@
+from scrapy import cmdline
+cmdline.execute(['scrapy', 'crawl', 'images'])
+
+
+print("===============================================================================================")
+# cmdline.execute(['scrapy', 'runspider', 'tutorial/spiders/quotes.py'])
