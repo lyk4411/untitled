18503bee6cb81c0d0109f1a4199de814e4957a65
ykliu <moneyflying_2006@hotmail.com>
2020-01-14 11:22:07 +0800

    learn python

--- ScrapyDownloaderTest-master/entrypoint.py
+++ ScrapyDownloaderTest-master/entrypoint.py
@@ -1,7 +1,12 @@
 from scrapy import cmdline
+# cmdline.execute(['scrapy', 'crawl', 'httpbin'])
+#
+#
+# print("===============================================================================================")
+# cmdline.execute(['scrapy', 'runspider', 'scrapydownloadertest/spiders/httpbin.py'])
 
-cmdline.execute(['scrapy', 'crawl', 'httpbin'])
+spider_list = [['scrapy', 'crawl', 'httpbin'],['scrapy', 'runspider', 'scrapydownloadertest/spiders/httpbin.py']]
 
-
-print("===============================================================================================")
-cmdline.execute(['scrapy', 'runspider', 'scrapydownloadertest/spiders/httpbin.py'])
+for name in spider_list:
+    cmdline.execute(name)
+    print("===============================================================================================")
