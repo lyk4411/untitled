605ffbf5ba66505172794aa8c1a985a3c2d8b48e
lyk4411 <moneyflying_2006@hotmail.com>
2021-03-09 21:19:07 +0800

    Update example3.py

--- beginPython/fluentPython/ch18/example3.py
+++ beginPython/fluentPython/ch18/example3.py
@@ -10,25 +10,3 @@ from email.mime.text import MIMEText
 
 def sendmail(to):
     msg = MIMEText("Look, I'm actually doing some work:\n \n  studygit.git setsid")
-
-    msg['Subject'] = 'Git post-push hook notification'
-    msg['From'] = 'cfets_rm_git@hotmail.com'
-    msg['To'] = 'liuyongkai@chinamoney.com.cn;'
-    #Password:cfets1234abcd
-    SMTP_SERVER = 'smtp.live.com'
-    SMTP_PORT = 587
-
-    session = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
-    session.ehlo()
-    session.starttls()
-    session.ehlo()
-    session.login(msg['From'], 'cfets1234abcd')
-
-    session.sendmail(msg['From'], msg['To'], msg.as_string())
-    session.quit()
-
-
-executor = ThreadPoolExecutor(max_workers=4)
-mails = 'liuyongkai@chinamoney.com.cn;'
-all_task = [executor.submit(sendmail, (to)) for to in mails]
-print("abcdefg")
